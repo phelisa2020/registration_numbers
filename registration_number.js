35d51c24fa783fc7e7e05be5d9af225edd26a1c4
phelisa2020 <phelisagqwetha@gmail.com>
2020-07-22 19:05:36 +0200

    my commits

--- registration_number.js
+++ registration_number.js
@@ -8,16 +8,32 @@ const regNumbersEntered = [];
 
 addRegNumberBtn.addEventListener("click", function() {
 //if nothing entered shows blank
-		const theReg = document.createElement("li");
+		const currentRegNumber = regNumberEntered.value.toUpperCase();
+if(currentRegNumber.startsWith('CY') ||
+	currentRegNumber.startsWith('CA') ||
+	currentRegNumber.startsWith('CJ')) {
 
-if (regNumberEntered.value) {
-	message.innerHTML = regNumberEntered.value; 
-} else  {
-	message.innerHTML = "";
+message.innerHTML = '';
+
+if(!regNumbersEntered.includes(currentRegNumber)){
+	// regNumberEntered.value;....it was this before
+
+	const regNumberElem = document.createElement('li');
+	regNumberElem.classList.add('regNumber');
+	regNumberElem.innerHTML = currentRegNumber
+	// regNumberEntered.value;....it was this before
+	regNumbers.appendChild(regNumberElem);
+
+	regNumbersEntered.push(currentRegNumber)
+
+	// regNumberEntered.value;....it was this before
+} 
+} else {
+	message.innerHTML = '';
+
 }
+	//regNumber.innerHTLM = regNumberEntered.value
+
+});
 
 
-regNumber.appendChild(theReg);
-
-
-	});
